10f6881f89b23095c177d3cf7d5227306ff94b09
MOESON <d.moeson@gmail.com>
2022-05-04 18:05:51 +0200

    Create config.php
    config.php model

---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php
+// config
+
+define('DB_USER',"username");
+define('DB_PASS',"password");
+define('DB_NAME',"gamma_software");
+define('DB_BASE',"mysql");
+define('DB_HOST',"127.0.0.1");
+
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', true);
+
